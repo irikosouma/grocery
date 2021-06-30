ffc199a781a670209366a8c7a0e7898ef475c184
ThanhIri <thanhnguyen@wee.vn>
2021-06-30 18:06:06 +0700

    up

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,31 +1,166 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 //styles
 import {WrapMain} from './index.styles'
+//images 
+import Edit from '../assets/images/edit'
+import Delete from '../assets/images/delete'
 export default function GroceryStore() {
+    
     const [valueInput, setValueInput] = useState("")
-    const [list, setList] = useState([])
+    const [isEmpty, setIsEmpty] = useState()
+    
+    const [indexEdit, setIndexEdit] = useState(-1);
+    const [list, setList] = useState(JSON.parse(localStorage.getItem('list')) || []);
     const inputRef = useRef(null)
-    const handleSubmit = (e) => {
-        let newVal = e.target.value
-        console.log('newval ne', newVal)
-        setValueInput(newVal)
-
+    
+    const handleSubmit = () => {
+        if(indexEdit !== -1) {
+            let newValue = inputRef.current
+            let newVal = newValue.value
+            const inputItem = {id: newVal, keyValue: newVal}
+            const newList = list.map((item, _) => 
+            item.id === indexEdit ? inputItem : item
+            )
+            setList(  newList)
+            setValueInput("")
+            setIndexEdit(-1)
+            setIsEmpty(2)
+            
+        } else {
+            if(valueInput === "") {
+                setList([...list])
+                setIsEmpty(3)
+            } else {
+                const inputItem = {id: valueInput, keyValue: valueInput}
+                setList( [inputItem, ...list] )
+                setIsEmpty(1)
+            }
+        }
+        {
+            setValueInput("")
+        }
     }
-    const handleChange = (e) => {
-        const newValue = e.target.value
+    const handleDelete = (idDelete) => {
+        const newList = list.filter((item, _) => item.id !== idDelete)
+        setList(newList)
+        let myStorage = window.localStorage
+        myStorage.removeItem(idDelete);
+        setIsEmpty(-1)
     }
+    const handleEdit = (idEdit) => {
+        setIndexEdit(idEdit)
+        setValueInput(idEdit)
+    }
+    const handleClearAll = () => {
+        setList([])
+        setIsEmpty(0)
+        myStorage.clear()
+    }
+    useEffect(() => {
+        return () => {
+            setTimeout(() => {
+                setIsEmpty()
+            }, 500)
+        }
+    }, [isEmpty])
+    let myStorage = window.localStorage
+    
+    if(list.length !== 0) {
+        myStorage.setItem('list', JSON.stringify(list))
+    }
+    //     // myStorage.setItem('list', JSON.stringify(list))
+    //     console.log('my storage', myStorage.getItem('list'))
+    
     return (
         <WrapMain>
-            <div className="title">Grocery Bud</div>
-            <div className="wrap-input">
-                <input ref={inputRef} id="input" type="text" placeholder="e.g. eggs" value={valueInput} onChange={(e) => handleChange(e)}/>
-                <button className="btn-submit" type="submit" onClick={() => handleSubmit(inputRef.current)}>Submit</button>
-            </div>
-            {list.map((item, index) => (
-                <div key={index}>
-                    {item}
-                </div>
-            ))}
+            
+            {
+                isEmpty === 1 ? 
+                <div className="add-announcement">Item Added To The List</div>
+                : 
+                ""
+            }
+            {
+                isEmpty === 2 ? 
+                <div className="add-announcement">Value Changed</div>
+                : 
+                ""
+            }
+            {
+                list.length === 0 && isEmpty === 0 ?
+                <div className="delete-announcement">Empty List</div>
+                :
+                ""
+            }
+            {
+                list.length === 0 && isEmpty === -1 ?
+                <div className="delete-announcement">Removed Item</div>
+                :
+                ""
+            } 
+            {
+                valueInput === "" && isEmpty === 3 ?
+                <div className="delete-announcement">Please Enter Value</div>
+                :
+                ""
+            }   
+            {
+                list.length === 0 ? 
+                <>
+                    <div className="title">Grocery Bud</div>
+                    <div className="wrap-input">
+                        <input ref={inputRef} id="input" type="text" placeholder="e.g. eggs" value={valueInput} onChange={(e) => setValueInput(e.target.value)} />
+                        <button className="btn-submit" type="submit" onClick={handleSubmit}>{indexEdit !== -1 ? "Edit" : "Submit" }</button>
+                    </div>
+                </>
+                :
+                <>
+                    <div className="title">Grocery Bud</div>
+                        <div className="wrap-input">
+                            <input ref={inputRef} id="input" type="text" placeholder="e.g. eggs" value={valueInput} onChange={(e) => setValueInput(e.target.value)} />
+                            <button className="btn-submit" type="submit" onClick={handleSubmit}>{indexEdit >= 0 ? "Edit" : "Submit" }</button>
+                        </div>
+                        {/* {localStorage.getItem(list)} */}
+                        {list.map((item, key) => (
+                            <div className="item-list" key={key}>
+                                <p className="item-style">
+                                    {item.keyValue}
+                                </p>
+                                <div className="btn-group">
+                                    <button className="btn-style">
+                                        <Edit className="edit-btn" onClick={() => handleEdit(item.id)}/>
+                                    </button>
+                                    <button className="btn-style" onClick={() => handleDelete(item.id)}>
+                                        <Delete className="delete-btn"/>
+                                    </button>
+                                    
+                                </div>
+                            </div>
+                            
+                        ))}
+                        <button className="btn-clear-all" onClick={handleClearAll}>Clear Items</button>
+                </>
+            
+            }
+        
+            
         </WrapMain>
     )
 }
+// {list.map((item, key) => (
+//     <div className="item-list" key={key}>
+//         <p className="item-style">
+//             {item.keyValue}
+//         </p>
+//         <div className="btn-group">
+//             <button className="btn-style">
+//                 <Edit className="edit-btn" onClick={() => handleEdit(item.id)}/>
+//             </button>
+//             <button className="btn-style" onClick={() => handleDelete(item.id)}>
+//                 <Delete className="delete-btn"/>
+//             </button>
+            
+//         </div>
+//     </div>
+    
+// ))}
--- src/Components/index.styles.js
+++ src/Components/index.styles.js
@@ -1,30 +1,48 @@
 import styled from 'styled-components'
 const WrapMain = styled.div`
-margin: 120px auto 300px auto;
-width: 500px;
+margin: 128px auto 300px auto;
+width: 90vw;
+max-width: 500px;
 height: calc(100% - 420px);
-display: flex;
-align-items: center;
-justify-content: center;
 background-color: #fff;
-color: #063252;
-font-size: 28px;
-line-height: 28px;
-font-weight: 700;
 display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: center;
-padding: 30px ;
+box-shadow: 0 5px 15px rgb(0 0 0 / 10%);
+/* width: 90vw; */
+/* margin: 8rem auto 0; */
+padding: 32px ;
+transition: all .3s linear;
+
+    .add-announcement, .delete-announcement {
+        background-color: #D4EDDA;
+        color: #155724;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 3px;
+        padding: 5px 0;
+        margin:5px 0 20px 0;
+    }
+    .delete-announcement {
+        background-color: #f8d7da;
+        color: #721c24;
+    }
     .title {
         margin-bottom: 30px;
+        color: #063252;
+        font-size: 28px;
+        line-height: 28px;
+        font-weight: 700;
     }
     .wrap-input {
         width: 100%;
         display: flex;
         align-items: center;
         justify-content: center;
-
+        margin-bottom: 30px;
         #input {
             flex: 1 1 auto;
             height: 28px;
@@ -41,8 +59,65 @@ padding: 30px ;
             outline: none;
             background-color: #a5d5f8;
             height: 30px;
+            width: 80px;
             border-radius: 0px 3px 3px 0;
+            transition: all 0.5s linear;
+            :hover {
+                background-color: #49A6E9;
+                color: #fff;
+            }
             
+        }
+    }
+    .item-list {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-bottom: 12px;
+        height: 35px;
+        border-radius: 3px;
+        :hover {
+            color: #617d98;
+            background-color: #F1F5F8;
+            
+        }
+        .item-style {
+            color: #102a42;
+            margin-left: 15px;
+        }
+    }
+    .btn-group {
+        margin-right: 15px;
+        .btn-style {
+            appearance: none;
+            outline: none;
+            border: none;
+            background: none;
+            .edit-btn {
+                fill: #6be675;
+                :hover {
+                    fill: #25bb32;
+                }
+            }
+            .delete-btn {
+                fill: #e66b6b;
+                :hover {
+                    fill: #bb2525;
+                }
+            }
+        }
+    }
+    .btn-clear-all {
+        color: #e66b6b;
+        
+        border: none;
+        background-color: transparent;
+        font-size:15px;
+        width: 156px;
+        height: 20px;
+        :hover {
+            color: #bb2525;
         }
     }
 `;
